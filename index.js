b5340888c239f10898c7d6f760995d920c02877e
Jomo245 <johnisige2020@gmail.com>
2025-04-18 04:32:49 +0300

    icon functions

---
+++ index.js
@@ -0,0 +1,17 @@
+let username=document.getElementById("username");
+const myCurrentPassword=document.getElementById("myCurrentPassword");
+const NewPassword=document.getElementById("NewPassword");
+const mySubmit=document.getElementById("mySubmit");
+
+
+mySubmit.onclick=function(){
+  
+ // myheading=document.getElementById("myheading").textContent='ENTER YOUR LOGINS DETAILS';
+  
+
+
+ 
+  
+
+  }
+
